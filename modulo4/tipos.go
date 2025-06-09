597eb66a9064eeb786cc6617d72305d4c0cc3046
Luanlmeida <luanlmeidaa7@gmail.com>
2025-06-09 18:07:52 -0300

    Update tipos.go

--- modulo4/tipos.go
+++ modulo4/tipos.go
@@ -1,1 +1,13 @@
 package main
+
+import (
+	"fmt"
+)
+
+func teste() {
+	fmt.Printf("Type: %T - Value %v\n", true, true)       //bool (true/false)
+	fmt.Printf("Type: %T - Value %v\n", "steph", "steph") //string (sequencia de bytes)
+	fmt.Printf("Type: %T - Value %v\n", 1, 1)             //int
+	fmt.Printf("Type: %T - Value %v\n", "1", "1")         //string
+	fmt.Printf("Type: %T - Value %v\n", 1.233, 1.233)     //float
+}
