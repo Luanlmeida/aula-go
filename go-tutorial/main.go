f98c22c7756e52ba9a9b158268bf77c72c599594
Luanlmeida <luanlmeidaa7@gmail.com>
2025-06-27 13:43:03 -0300

    Update main.go

--- go-tutorial/main.go
+++ go-tutorial/main.go
@@ -3,5 +3,6 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World!")
+	fmt.Println("Welcome to a conference booking application")
+	fmt.Println("Get your tickets here to attend")
 }
