ce403787c2cf3acde2175ff21667a89516f2fbab
Luanlmeida <luanlmeidaa7@gmail.com>
2025-06-27 14:02:53 -0300

    Update main.go

--- go-tutorial/main.go
+++ go-tutorial/main.go
@@ -10,4 +10,11 @@ func main() {
 	fmt.Printf("Welcome to %v booking application\n", conferenceName)
 	fmt.Printf("We have total of %v tickets and %v are still avalible", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
+
+	var userName string
+	var userTickets int
+
+	userName = "Luan"
+	userTickets = 2
+	fmt.Printf("User %v booked %v tickets.\n", userName, userTickets)
 }
