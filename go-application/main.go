4eee0fcc4ba42801612f0a1b273666c5f1a76238
Luanlmeida <luanlmeidaa7@gmail.com>
2025-07-07 12:53:14 -0300

    Update main.go

--- go-application/main.go
+++ go-application/main.go
@@ -2,14 +2,13 @@ package main
 
 import (
 	"fmt"
-	"strconv"
 )
 
 const conferenceTickets int = 50
 
 var conferenceName string = "Go Conference"
 var remainingTickets uint = 50 // u = valor não pode ser negativo
-var bookings = make([]map[string]string, 0)
+var bookings = make([]UserData, 0)
 
 type UserData struct {
 	firstName      string
@@ -61,7 +60,7 @@ func greetUsers() {
 func getFirstNames() []string {
 	firstNames := []string{}
 	for _, booking := range bookings {
-		firstNames = append(firstNames, booking["firstName"])
+		firstNames = append(firstNames, booking.firstName)
 	}
 	return firstNames
 }
@@ -90,11 +89,12 @@ func getUserInput() (string, string, string, uint) {
 func bookTicket(userTickets uint, firstName string, lastName string, email string) {
 	remainingTickets = remainingTickets - userTickets
 
-	var userData = make(map[string]string)
-	userData["firstName"] = firstName
-	userData["lastName"] = lastName
-	userData["email"] = email
-	userData["numberOfTickets"] = strconv.FormatUint(uint64(userTickets), 10)
+	var userData = UserData{
+		firstName:      firstName,
+		lastName:       lastName,
+		email:          email,
+		numerOfTickets: userTickets,
+	}
 
 	bookings = append(bookings, userData)
 	fmt.Printf("List of bookings is %v\n", bookings)
