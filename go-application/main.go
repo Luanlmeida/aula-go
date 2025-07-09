90538c383048f1e6130d5ebfe32e253c0e22561e
Luanlmeida <luanlmeidaa7@gmail.com>
2025-07-09 12:34:03 -0300

    Update main.go

--- go-application/main.go
+++ go-application/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 const conferenceTickets int = 50
 
@@ -27,6 +30,7 @@ func main() {
 		if isValidName && isValidEmail && isValidTicketNumber {
 
 			bookTicket(userTickets, firstName, lastName, email)
+			sendTicket(userTickets, firstName, lastName, email)
 
 			firstNames := getFirstNames()
 			fmt.Printf("The first names of bookings are: %v\n", firstNames)
@@ -99,4 +103,12 @@ func bookTicket(userTickets uint, firstName string, lastName string, email strin
 
 	fmt.Printf("Thank you %v %v for booked %v tickets. you be receive a confirmation on email at %v\n", firstName, lastName, userTickets, email)
 	fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
+
+func sendTicket(userTickets uint, firstName string,lastName string, email string) {
+	time.Sleep(10 * time.Second)
+	var ticket = fmt.Sprintf("%v tickets for %v %v", userTickets, firstName,lastName)
+	fmt.Println("################")
+	fmt.Printf("Sending ticket:\n %v \nto email address %v\n", userTickets, email)
+	fmt.Println("################")
+}
 }
